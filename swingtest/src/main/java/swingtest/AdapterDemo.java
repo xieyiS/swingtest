a9cbf5da8f3abec7e3839abb2eee451ed632e77d
Administrator <Administrator@WINDOWS-P3B5ISF>
2019-04-24 13:40:41 +0800

    jdk1.8

---
+++ swingtest/src/main/java/swingtest/AdapterDemo.java
@@ -0,0 +1,25 @@
+package swingtest;
+
+import javax.swing.JFrame;
+
+public class AdapterDemo extends JFrame {
+	/*AdapterDemo() {
+	    addWindowListener(new MyAdapter());
+	}*/
+
+    public static void main(String[] args) {
+		/*AdapterDemo frame = new AdapterDemo();
+		frame.setTitle("我的框架");
+		frame.setSize(400, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);*/
+    }
+
+	/*class MyAdapter extends WindowAdapter {
+	    public void windowActivated(WindowEvent e) {
+	        System.out.println("windowActivated....");
+	    }
+	}*/
+
+}
